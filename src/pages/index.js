7be1c0cba2ae8d82c2bec0cd8b98ba3c4fd1485a
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-05-18 09:49:00 -0300

    check lint

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const a = 'a';
+  const a = 'a'dsa
   return (
     <div className={styles.container}>
       <Head>
