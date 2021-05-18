e58b537146afda4d2afed144f5f753cd2b0c40a4
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-05-18 10:02:57 -0300

    lint stagged work

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const a = 'a' dasd;
   return (
     <div className={styles.container}>
       <Head>
