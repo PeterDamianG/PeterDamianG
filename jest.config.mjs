f0a7d3f1d78d07820e93f7e0adcd654834322854
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-05-27 16:51:09 -0300

    Divide globals into globals and themes.

--- jest.config.mjs
+++ jest.config.mjs
@@ -16,6 +16,7 @@ export default {
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+  moduleDirectories: ['node_modules', 'src'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
