2b94431fbf36d7373167fb2b55bfedd4d7ac6ae3
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-06-07 04:08:51 -0300

    refactor home section

---
+++ src/components/main/sections/hero/components/objectContent.js
@@ -0,0 +1,49 @@
+/** @module Main/Sections/Hero */
+/**
+ * A var type const for set info to display.
+ * @const
+ * @example
+ * objectContent[0].notebook
+ * return "<HTML>"
+ * @returns {Array} An array of objects.
+ */
+const objectContent = [
+  {
+    notebook: '<HTML>',
+    x: '750',
+    y: '650',
+    caption: `"Me pregunto. ¿Cómo pasamos de tres archivos estáticos, a diez sólo de configuración?"`,
+  },
+  {
+    notebook: '<Script>',
+    x: '750',
+    y: '650',
+    caption: `"Me pregunto. ¿Cuántas veces habré iterado sobre esté código?"`,
+  },
+  {
+    notebook: '<Style>',
+    x: '750',
+    y: '650',
+    caption: `"Me pregunto. ¿Es realmente necesario el CSS-in-JSS?"`,
+  },
+  {
+    notebook: '<Main>',
+    x: '750',
+    y: '650',
+    caption: `"Me pregunto. ¿Cuándo la semantica cambie, re-adaptaremos todas las webs?"`,
+  },
+  {
+    notebook: '<br />',
+    x: '800',
+    y: '650',
+    caption: `"Me pregunto. ¿Hace cuánto que no utilizo algunas etiquetas?"`,
+  },
+  {
+    notebook: '<SVG>',
+    x: '800',
+    y: '650',
+    caption: `"Me pregunto. ¿Cuándo aparecerá el sistema de graficos definitivo?"`,
+  },
+];
+
+export default objectContent;
--- src/components/main/sections/hero/index.js
+++ src/components/main/sections/hero/index.js
@@ -3,61 +3,16 @@ import { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import SocialMedia from 'components/utils/socialmedia';
 import ScrollDownSVG from 'components/icons/utils/scrolldown/ScrollDownSVG';
-import NotebookSVG from './NotebookSVG';
+import NotebookSVG from './components/NotebookSVG';
 import { sectionStyle } from './hero.module.css';
-/**
- * A var type const for set info to display.
- * @const
- * @example
- * objectContent[0].notebook
- * return "<HTML>"
- * @returns {Array} An array of objects.
- */
-const objectContent = [
-  {
-    notebook: '<HTML>',
-    x: '750',
-    y: '650',
-    caption: `"Me pregunto. ¿Cómo pasamos de tres archivos estáticos, a diez sólo de configuración?"`,
-  },
-  {
-    notebook: '<Script>',
-    x: '750',
-    y: '650',
-    caption: `"Me pregunto. ¿Cuántas veces habré iterado sobre esté código?"`,
-  },
-  {
-    notebook: '<Style>',
-    x: '750',
-    y: '650',
-    caption: `"Me pregunto. ¿Es realmente necesario el CSS-in-JSS?"`,
-  },
-  {
-    notebook: '<Main>',
-    x: '750',
-    y: '650',
-    caption: `"Me pregunto. ¿Cuándo la semantica cambie, re-adaptaremos todas las webs?"`,
-  },
-  {
-    notebook: '<br />',
-    x: '800',
-    y: '650',
-    caption: `"Me pregunto. ¿Hace cuánto que no utilizo algunas etiquetas?"`,
-  },
-  {
-    notebook: '<SVG>',
-    x: '800',
-    y: '650',
-    caption: `"Me pregunto. ¿Cuándo aparecerá el sistema de graficos definitivo?"`,
-  },
-];
+import objectContent from './components/objectContent';
 /**
  * A function to get a random number of array objectContent.
  * @function getRandomNumber
  * @example
  * getRandomNumber()
- * return 2
- * @returns {Number} An integer number.
+ * // return 2
+ * @returns {Number} An integer number of length to objectContent.
  */
 const getRandomNumber = () => Math.floor(Math.random() * objectContent.length);
 /**
