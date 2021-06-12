7e9a76602cc0be55c5e15fc4c3268ef22541c750
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-06-12 06:10:12 -0300

    tests for section hero

--- src/components/main/sections/hero/Hero.test.js
+++ src/components/main/sections/hero/Hero.test.js
@@ -1,10 +1,11 @@
 import { render, screen, cleanup, fireEvent } from '@testing-library/react';
+import { Context as ResponsiveContext } from 'react-responsive';
 import Hero from './index';
 
-beforeEach(() => render(<Hero />));
 afterAll(cleanup);
 
-describe('/src/components/main/section/hero - <Hero>', () => {
+describe('/src/components/main/section/hero - <Hero> - Renders', () => {
+  beforeEach(() => render(<Hero />));
   test('does render section home', () => {
     screen.getByText(/Peter Damián Gómez/i);
     screen.getByText(/Web Developer/i);
@@ -16,6 +17,28 @@ describe('/src/components/main/section/hero - <Hero>', () => {
     screen.getByLabelText(/Scroll Down/i);
   });
   test('Does renders new content when click figure notebook', () => {
+    fireEvent.click(screen.getByLabelText(/Notebook SVG/i));
+    screen.getByText(/Me pregunto/i);
+  });
+});
+
+describe('/src/components/main/section/hero - <Hero> - Responsive', () => {
+  beforeEach(() => render(
+    <ResponsiveContext.Provider value={{ width: 1280 }}>
+      <Hero />
+    </ResponsiveContext.Provider>,
+  ));
+  test('Does renders home in screen width >= 1280', () => {
+    screen.getByText(/Peter Damián Gómez/i);
+    screen.getByText(/Web Developer/i);
+    screen.getByLabelText(/GitHub Button/);
+    screen.getByLabelText(/GoogleMail Button/);
+    screen.getByLabelText(/LinkedIn Button/);
+    screen.getByText(/Me pregunto/i);
+    screen.getByLabelText(/Notebook SVG/i);
+    screen.getByLabelText(/Scroll Down/i);
+  });
+  test('Does renders new content when click figure notebook in screen width >= 1280', () => {
     fireEvent.click(screen.getByLabelText(/Notebook SVG/i));
     screen.getByText(/Me pregunto/i);
   });
