dac88c83c57a2a0f140d5cf1194f54df350dab33
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-06-12 08:54:29 -0300

    add test for contact

---
+++ src/components/main/sections/contact/Contact.test.js
@@ -0,0 +1,18 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import Contact from './index';
+
+afterAll(cleanup);
+
+describe('/src/components/main/section/contact - <Contact> - Render', () => {
+  test('does render section contact', () => {
+    render(<Contact />);
+    screen.getByText(/Contacto/i);
+    screen.getByText(/¿Y ahora, qué?/i);
+    screen.getByText(/¿Quiéres contactarme?/i);
+    screen.getByLabelText(/GitHub Button/);
+    screen.getByLabelText(/GoogleMail Button/);
+    screen.getByLabelText(/LinkedIn Button/);
+    screen.getByText(/¿Necesitas un CV o Resume?/i);
+    screen.getByText(/Gracias por su atención./i);
+  });
+});
