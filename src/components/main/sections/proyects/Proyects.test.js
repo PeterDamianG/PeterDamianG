9389c74fb1136f5b26aa00f7ee1fbdc0572ff2f5
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-06-12 08:32:06 -0300

    add test for section proyects

--- src/components/main/sections/proyects/Proyects.test.js
+++ src/components/main/sections/proyects/Proyects.test.js
@@ -1,7 +1,7 @@
 import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import { Context as ResponsiveContext } from 'react-responsive';
 import Proyects from './index';
-import listProyects from "./setproyects/list/listProyects";
+import listProyects from './setproyects/list/listProyects';
 
 afterAll(cleanup);
 
@@ -13,20 +13,20 @@ describe('/src/components/main/section/proyects - <Proyects> - Render', () => {
     screen.getByText(listProyects[0].lang);
     screen.getByAltText(listProyects[0].name);
     screen.getByText(listProyects[0].desc);
-    screen.getByText("<");
+    screen.getByText('<');
     screen.getByText(/¡Ir a GitHub!/i);
-    screen.getByText(">");
+    screen.getByText('>');
     screen.getByLabelText(/Scroll Down/i);
   });
   test('Does renders new content when click on next button', () => {
-    fireEvent.click(screen.getByText(">"));
+    fireEvent.click(screen.getByText('>'));
     screen.getByText(listProyects[1].name);
     screen.getByText(listProyects[1].lang);
     screen.getByAltText(listProyects[1].name);
     screen.getByText(listProyects[1].desc);
   });
   test('Does renders new content when click on previous button', () => {
-    fireEvent.click(screen.getByText("<"));
+    fireEvent.click(screen.getByText('<'));
     screen.getByText(listProyects[listProyects.length - 1].name);
     screen.getByText(listProyects[listProyects.length - 1].lang);
     screen.getByAltText(listProyects[listProyects.length - 1].name);
@@ -48,20 +48,20 @@ describe('/src/components/main/section/about - <Proyects> - Responsive', () => {
     screen.getByText(listProyects[0].lang);
     screen.getByAltText(listProyects[0].name);
     screen.getByText(listProyects[0].desc);
-    screen.getByText("<");
+    screen.getByText('<');
     screen.getByText(/¡Ir a GitHub!/i);
-    screen.getByText(">");
+    screen.getByText('>');
     screen.getByLabelText(/Scroll Down/i);
   });
   test('Does renders new content when click on next button in screen width >= 1280', () => {
-    fireEvent.click(screen.getByText(">"));
+    fireEvent.click(screen.getByText('>'));
     screen.getByText(listProyects[1].name);
     screen.getByText(listProyects[1].lang);
     screen.getByAltText(listProyects[1].name);
     screen.getByText(listProyects[1].desc);
   });
   test('Does renders new content when click on previous button in screen width >= 1280', () => {
-    fireEvent.click(screen.getByText("<"));
+    fireEvent.click(screen.getByText('<'));
     screen.getByText(listProyects[listProyects.length - 1].name);
     screen.getByText(listProyects[listProyects.length - 1].lang);
     screen.getByAltText(listProyects[listProyects.length - 1].name);
