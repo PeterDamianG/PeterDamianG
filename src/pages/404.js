9389eea81e5e58d3f2271a588376fd684580340c
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-06-12 11:06:56 -0300

    working on 404 page not found and redirect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,17 @@
-const Page404 = () => <h1>404 - Page Not Found</h1>;
+/** @module Pages */
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+/**
+ * A component for redirect to index.
+ * @function Page404
+ * @returns {JSX.Element} An element of React.
+ */
+const Page404 = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/');
+  }, []);
+  return <h1>404 - Page Not Found</h1>;
+};
 
 export default Page404;
