79c895abbbc66331befc1e3abe6e9ac9321d76f9
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-06-12 07:12:31 -0300

    test for section about

--- src/components/main/sections/about/About.test.js
+++ src/components/main/sections/about/About.test.js
@@ -1,12 +1,37 @@
 import { render, screen, cleanup } from '@testing-library/react';
+import { Context as ResponsiveContext } from 'react-responsive';
 import About from './index';
 
 afterAll(cleanup);
 
-describe('/src/components/main/section/about - <About>', () => {
+describe('/src/components/main/section/about - <About> - Render', () => {
   test('does render section about', () => {
     render(<About />);
     screen.getByText(/Sobre Mí/i);
+    screen.getByLabelText(/Avatar/i);
+    screen.getByText(/¡Hola!, ¡Que estés teniendo un buen día!/i);
+    screen.getByText(/Iterando siempre sobre cada detalle./i);
+    screen.getByText(/¡Pero no son todos!:/i);
+    screen.getByText(/ReactJS/i);
+    screen.getByText(/NextJS/i);
     screen.getByLabelText(/Scroll Down/i);
   });
 });
+
+describe('/src/components/main/section/about - <About> - Responsive', () => {
+  test('does render section about', () => {
+    render(
+      <ResponsiveContext.Provider value={{ width: 1280 }}>
+        <About />
+      </ResponsiveContext.Provider>,
+    );
+    screen.getByText(/Sobre Mí/i);
+    screen.getByLabelText(/Avatar/i);
+    screen.getByText(/¡Hola!, ¡Que estés teniendo un buen día!/i);
+    screen.getByText(/Iterando siempre sobre cada detalle./i);
+    screen.getByText(/¡Pero no son todos!:/i);
+    screen.getByText(/ReactJS/i);
+    screen.getByText(/NextJS/i);
+    screen.getByLabelText(/Scroll Down/i);
+  });
+})
