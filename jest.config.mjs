4a5ea4fe7b0d6f631b4d0b046bf0b2b9247b865d
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-06-13 07:21:35 -0300

    remove unneccesary file

--- jest.config.mjs
+++ jest.config.mjs
@@ -12,7 +12,6 @@ export default {
     '<rootDir>/.husky/',
     '<rootDir>/cypress/',
   ],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
+/** @module Pages */
 import Head from 'next/head';
 import AppBar from 'components/layout/appbar';
 import Main from 'components/main';
-
+/**
+ * A component home is entry of page web.
+ * @function Home
+ * @returns {JSX.Element} An element of React.
+ */
 const Home = () => (
   <>
     <Head>
       <title>Peter Damián G - Personal Web</title>
       <meta
         name='description'
-        content='A personal web site. Peter Damian G is a software web developer.'
+        content='Sitio Personal del desarrollador web, Peter Damián Gómez.'
       />
     </Head>
     <AppBar />
