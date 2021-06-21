4f6d1384fe9894c2579103f7ac6ef1d71090b088
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-06-21 12:24:25 -0300

    Fix img to image component of nextjs

--- src/components/main/sections/about/avatar/Avatar.js
+++ src/components/main/sections/about/avatar/Avatar.js
@@ -1,4 +1,6 @@
 /** @module Sections/About */
+import Image from 'next/image';
+import { useMediaQuery } from 'react-responsive';
 import { avatarStyle } from './avatar.module.css';
 /**
  * A component to render Avatar.
@@ -8,13 +10,48 @@ import { avatarStyle } from './avatar.module.css';
  * <Avatar />
  * @returns {JSX.Element} An element of React.
  */
-const Avatar = () => (
-  <img
-    aria-label='Avatar'
-    alt='Peter DG'
-    src='./me.png'
-    className={avatarStyle}
-  />
-);
+const Avatar = () => {
+  // Design with full media.
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1280px)',
+  });
+  // Design for medium device like a tablet.
+  const isMediumDevice = useMediaQuery({ minWidth: 640, maxWidth: 1280 });
+  if (isDesktopOrLaptop)
+    return (
+      <Image
+        aria-label='Avatar'
+        alt='Picture of Author. Peter DG.'
+        src='/me.png'
+        width={150}
+        height={150}
+        priority
+        className={avatarStyle}
+      />
+    );
+  if (isMediumDevice)
+    return (
+      <Image
+        aria-label='Avatar'
+        alt='Picture of Author. Peter DG.'
+        src='/me.png'
+        width={125}
+        height={125}
+        priority
+        className={avatarStyle}
+      />
+    );
+  return (
+    <Image
+      aria-label='Avatar'
+      alt='Picture of Author. Peter DG.'
+      src='/me.png'
+      width={100}
+      height={100}
+      priority
+      className={avatarStyle}
+    />
+  );
+};
 
 export default Avatar;
