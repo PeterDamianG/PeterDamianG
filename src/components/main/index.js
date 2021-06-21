53458a929407c979578cce5c9abc8d8b61f5b470
Peter Damián Gómez <PeterDamianG@users.noreply.github.com>
2021-06-21 18:42:09 -0300

    fix error
    Expected server HTML to contain a matching <tag> in <tag>.
    
    We need back to ssr:false

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,22 +1,21 @@
 /** @module Sections */
 import dynamic from 'next/dynamic';
-import LoaderSpinner from 'components/icons/utils/LoaderSpinner';
 import { mainStyle } from './main.module.css';
 /** @constant {JSX.Element} */
 const Hero = dynamic(() => import('components/main/sections/hero'), {
-  loading: <LoaderSpinner />,
+  ssr: false,
 });
 /** @constant {JSX.Element} */
 const About = dynamic(() => import('components/main/sections/about'), {
-  loading: <LoaderSpinner />,
+  ssr: false,
 });
 /** @constant {JSX.Element} */
 const Proyects = dynamic(() => import('components/main/sections/proyects'), {
-  loading: <LoaderSpinner />,
+  ssr: false,
 });
 /** @constant {JSX.Element} */
 const Contact = dynamic(() => import('components/main/sections/contact'), {
-  loading: <LoaderSpinner />,
+  ssr: false,
 });
 /**
  * A tag main container for sections of the web.
@@ -28,10 +27,10 @@ const Contact = dynamic(() => import('components/main/sections/contact'), {
  */
 const Main = () => (
   <main className={mainStyle}>
-    <Hero />
-    <About />
-    <Proyects />
-    <Contact />
+        <Hero />
+        <About />
+        <Proyects />
+        <Contact />
   </main>
 );
 
