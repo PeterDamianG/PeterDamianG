2df0553d698933735470b6484d646ab073dfcb40
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-08 22:18:40 -0300

    we need first fix contact section styles

--- src/components/main/sections/proyects/setproyects/card/Card.js
+++ src/components/main/sections/proyects/setproyects/card/Card.js
@@ -1,9 +1,7 @@
 /** @module Sections/Proyects */
-import { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import animationHelper from 'components/utils/animationHelper';
 import Image from 'next/image';
-import { container, animateStyle } from './card.module.css';
+import { container } from './card.module.css';
 /**
  * A component to render cards.
  * @function Card
@@ -13,27 +11,24 @@ import { container, animateStyle } from './card.module.css';
  * @returns {JSX.Element} An element of React.
  */
 const Card = ({ name, url, desc, lang, img, upIndex, downIndex }) => {
-  // State for handle animation.
-  const [animate, setAnimate] = useState(false);
   // Handle Click Arrow.
   const handleClickArrow = (upOrDown) => {
-    animationHelper(setAnimate, 1000);
     upOrDown();
   };
-  // Other design with full media.
+  // Other design.
   const isDesktopOrLaptop = useMediaQuery({
     query: '(min-width: 1280px)',
   });
-  // Design for medium device like a tablet.
   const isMediumDevice = useMediaQuery({ minWidth: 640, maxWidth: 1280 });
+  const isLandscape = useMediaQuery({ orientation: 'landscape' });
   // Render if width is >= 1280px.
   if (isDesktopOrLaptop) {
     return (
       <article className={container}>
         <article>
-          <h3 className={animate ? animateStyle : ''}>{name}</h3>
-          <h4 className={animate ? animateStyle : ''}>{lang}</h4>
-          <p className={animate ? animateStyle : ''}>{desc}</p>
+          <h3>{name}</h3>
+          <h4>{lang}</h4>
+          <p>{desc}</p>
           <nav>
             <button type='button' onClick={() => handleClickArrow(downIndex)}>
               &lt;
@@ -46,29 +41,22 @@ const Card = ({ name, url, desc, lang, img, upIndex, downIndex }) => {
             </button>
           </nav>
         </article>
-        <Image
-          className={animate ? animateStyle : ''}
-          src={img}
-          alt={name}
-          width={800}
-          height={800}
-        />
+        <Image src={img} alt={name} width={800} height={800} />
       </article>
     );
   }
   // Render Normal.
   return (
     <article className={container}>
-      <h3 className={animate ? animateStyle : ''}>{name}</h3>
-      <h4 className={animate ? animateStyle : ''}>{lang}</h4>
+      {isLandscape || <h3>{name}</h3>}
+      <h4>{lang}</h4>
       <Image
-        className={animate ? animateStyle : ''}
         src={img}
         alt={name}
         width={isMediumDevice ? 550 : 300}
         height={isMediumDevice ? 600 : 400}
       />
-      <p className={animate ? animateStyle : ''}>{desc}</p>
+      <p>{desc}</p>
       <nav>
         <button type='button' onClick={() => handleClickArrow(downIndex)}>
           &lt;
