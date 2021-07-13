e4f56ca01d596af3885dbb4939a01775f7eeb2df
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-13 16:34:39 -0300

    add hotkeys for sections move.

--- src/layout/appbar/drawer/index.js
+++ src/layout/appbar/drawer/index.js
@@ -18,6 +18,19 @@ const Drawer = () => {
   const closeDrawer = () => setIsOpen(false);
   // Set hotkey to open/close drawer.
   useHotkeys('m', () => setIsOpen((open) => !open));
+  // Set hotkey to handler move between sections.
+  useHotkeys('h', () => {
+    window.location.hash = 'hero'; // Go hero section.
+  });
+  useHotkeys('a', () => {
+    window.location.hash = 'about'; // Go about section.
+  });
+  useHotkeys('p', () => {
+    window.location.hash = 'proyects'; // Go proyects section.
+  });
+  useHotkeys('c', () => {
+    window.location.hash = 'contact'; // Go contact section.
+  });
   // Render Component
   return (
     <>
