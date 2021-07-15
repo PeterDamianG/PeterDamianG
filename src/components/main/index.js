294d9c3e2c47d85603335a78d6aa1061046cff8d
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-15 11:58:18 -0300

    fix error dinamic charge reacttooltips

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,4 +1,6 @@
 /** @module Sections */
+import { useTheme } from 'next-themes';
+import ReactTooltip from 'react-tooltip';
 import { mainStyle } from './main.module.css';
 import Hero from './sections/hero';
 import About from './sections/about';
@@ -12,13 +14,24 @@ import Contact from './sections/contact';
  * <Main />
  * @returns {JSX.Element} An element of React.
  */
-const Main = () => (
-  <main className={mainStyle}>
-    <Hero />
-    <About />
-    <Proyects />
-    <Contact />
-  </main>
-);
+const Main = () => {
+  const { theme } = useTheme();
+  return (
+    <main className={mainStyle}>
+      <Hero />
+      <About />
+      <Proyects />
+      <Contact />
+      <ReactTooltip
+        place='bottom'
+        delayShow={600}
+        delayHide={200}
+        type={theme}
+        border
+        className='extraToolTip'
+      />
+    </main>
+  );
+};
 
 export default Main;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,9 @@
 /** @module Pages */
 import dynamic from 'next/dynamic';
 import LoaderSpinner from 'components/icons/utils/LoaderSpinner';
-import { useTheme } from 'next-themes';
 import Head from 'next/head';
 /** @constant {JSX.Element} */
 const Main = dynamic(() => import('components/main'), {
-  loading: () => <LoaderSpinner />,
-  ssr: false,
-});
-/** @constant {JSX.Element} */
-const ReactTooltip = dynamic(() => import('react-tooltip'), {
   loading: () => <LoaderSpinner />,
   ssr: false,
 });
@@ -18,28 +12,17 @@ const ReactTooltip = dynamic(() => import('react-tooltip'), {
  * A component home is entry of page web.
  * @function Home
  */
-const Home = () => {
-  const { theme } = useTheme();
-  return (
-    <>
-      <Head>
-        <title>Peter Damián G - Personal Web</title>
-        <meta
-          name='description'
-          content='Sitio Personal del desarrollador web, Peter Damián Gómez.'
-        />
-      </Head>
-      <Main />
-      <ReactTooltip
-        place='bottom'
-        delayShow={600}
-        delayHide={200}
-        type={theme}
-        border
-        className='extraToolTip'
+const Home = () => (
+  <>
+    <Head>
+      <title>Peter Damián G - Personal Web</title>
+      <meta
+        name='description'
+        content='Sitio Personal del desarrollador web, Peter Damián Gómez.'
       />
-    </>
-  );
-};
+    </Head>
+    <Main />
+  </>
+);
 
 export default Home;
