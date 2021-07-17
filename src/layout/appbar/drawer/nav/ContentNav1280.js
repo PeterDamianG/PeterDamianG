e40c17af986e43e8fe5b2a56c2fb38036968ea16
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-17 19:42:28 -0300

    add state and function to show text content

--- src/layout/appbar/drawer/nav/ContentNav1280.js
+++ src/layout/appbar/drawer/nav/ContentNav1280.js
@@ -1,5 +1,8 @@
 /** @module Layout/AppBar */
+import { useState } from 'react';
+import useFunction from 'hooks/useFunction';
 import SocialMedia from 'components/utils/socialmedia';
+import textContent from './textContent';
 import {
   sidenav,
   closebtn,
@@ -8,6 +11,7 @@ import {
   proyects,
   contact,
   detail,
+  textContentStyle,
 } from './contentnav.module.css';
 /**
  * A content of navigation when you have in 1280 or more of resolution width.
@@ -17,63 +21,80 @@ import {
  * import ContentNav from 'components/layout/appbar/drawer/ContentNav1280'
  * <ContentNav1280 close={handleClose} />
  */
-const ContentNav1280 = ({ close }) => (
-  <nav className={sidenav}>
-    <button
-      data-tip='Cierra el menú de navegación.'
-      type='button'
-      className={closebtn}
-      onClick={close}
-    >
-      X
-    </button>
-    <article className={hero}>
-      <a
-        data-tip='Presiona H, para acceder rápidamente a esa sección.'
-        href='#hero'
+const ContentNav1280 = ({ close }) => {
+  // State for show/change textContent.
+  const [number, setNumber] = useState(0);
+  // Handler click on TextContent
+  const handlerClickText = () => {
+    if (number === textContent.length - 1) setNumber(0);
+    else setNumber((prevNumber) => prevNumber + 1);
+  };
+  // Hook to change states automatic.
+  useFunction(handlerClickText, 10000);
+  return (
+    <nav className={sidenav}>
+      <button
+        data-tip='Cierra el menú de navegación.'
+        type='button'
+        className={closebtn}
         onClick={close}
       >
-        Inicio
-      </a>
-      <p>Inicio de la web, donde les comparto algunos pensamientos.</p>
-    </article>
-    <article className={about}>
-      <a
-        data-tip='Presiona A, para acceder rápidamente a esa sección.'
-        href='#about'
-        onClick={close}
-      >
-        Sobre Mí
-      </a>
-      <p>Conoceme más. Algunos detalles más de mí persona.</p>
-    </article>
-    <article className={proyects}>
-      <a
-        data-tip='Presiona P, para acceder rápidamente a esa sección.'
-        href='#proyects'
-        onClick={close}
-      >
-        Proyectos
-      </a>
-      <p>Los proyectos que he llevado a cabo. No todos ellos.</p>
-    </article>
-    <article className={contact}>
-      <a
-        data-tip='Presiona C, para acceder rápidamente a esa sección.'
-        href='#contact'
-        onClick={close}
-      >
-        Contacto
-      </a>
-      <p>Sí estás interesado en contactarme, aquí las mejores opciones.</p>
-    </article>
-    <article className={detail}>
-      <h3>Peter DG</h3>
-      <SocialMedia />
-      <p>No olvides de adaptarte a tú resolución preferida.</p>
-      <small>© {new Date().getFullYear()} Peter DG.</small>
-    </article>
-  </nav>
-);
+        X
+      </button>
+      <article className={hero}>
+        <a
+          data-tip='Presiona H, para acceder rápidamente a esa sección.'
+          href='#hero'
+          onClick={close}
+        >
+          Inicio
+        </a>
+        <p>Inicio de la web, donde les comparto algunos pensamientos.</p>
+      </article>
+      <article className={about}>
+        <a
+          data-tip='Presiona A, para acceder rápidamente a esa sección.'
+          href='#about'
+          onClick={close}
+        >
+          Sobre Mí
+        </a>
+        <p>Conoceme más. Algunos detalles más de mí persona.</p>
+      </article>
+      <article className={proyects}>
+        <a
+          data-tip='Presiona P, para acceder rápidamente a esa sección.'
+          href='#proyects'
+          onClick={close}
+        >
+          Proyectos
+        </a>
+        <p>Los proyectos que he llevado a cabo. No todos ellos.</p>
+      </article>
+      <article className={contact}>
+        <a
+          data-tip='Presiona C, para acceder rápidamente a esa sección.'
+          href='#contact'
+          onClick={close}
+        >
+          Contacto
+        </a>
+        <p>Sí estás interesado en contactarme, aquí las mejores opciones.</p>
+      </article>
+      <article className={detail}>
+        <h3>Peter DG</h3>
+        <SocialMedia />
+        <p
+          className={textContentStyle}
+          onClick={handlerClickText}
+          aria-hidden='true'
+        >
+          No olvides de adaptarte a tú resolución preferida.
+        </p>
+        <small>© {new Date().getFullYear()} Peter DG.</small>
+      </article>
+    </nav>
+  );
+};
 
 export default ContentNav1280;
