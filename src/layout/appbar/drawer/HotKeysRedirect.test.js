b3d8961dc172cacd7a372d0d786ddd90b50adbac
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-17 18:49:23 -0300

    add test for hotkeys shortcut navs

---
+++ src/layout/appbar/drawer/HotKeysRedirect.test.js
@@ -0,0 +1,30 @@
+import { render, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Drawer from '.';
+
+beforeEach(() => {
+  delete window.location;
+  window.location = new URL('https://www.mock.com');
+  render(<Drawer />);
+});
+
+afterAll(cleanup);
+
+describe('/src/components/layout/appbar/Drawer.js - <Drawer> - HotKeys Nav', () => {
+  test('Does HotKeys "H" redirect to section home working.', () => {
+    userEvent.keyboard('H');
+    expect(window.location.hash).toBe('#hero');
+  });
+  test('Does HotKeys "A" redirect to section about working.', () => {
+    userEvent.keyboard('A');
+    expect(window.location.hash).toBe('#about');
+  });
+  test('Does HotKeys "P" redirect to section proyects working.', () => {
+    userEvent.keyboard('P');
+    expect(window.location.hash).toBe('#proyects');
+  });
+  test('Does HotKeys "C" redirect to section contact working.', () => {
+    userEvent.keyboard('C');
+    expect(window.location.hash).toBe('#contact');
+  });
+});
