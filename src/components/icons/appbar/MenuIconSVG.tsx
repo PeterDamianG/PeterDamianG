645bb7ecaa998626bcfeed00997f5ac7df3ff0c3
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-09 17:38:35 -0300

    icons app bar to typescript

---
+++ src/components/icons/appbar/MenuIconSVG.tsx
@@ -0,0 +1,23 @@
+import { SVGProps } from 'react';
+/**
+ * Use a icon SVG like a menu button.
+ * @function MenuIconSVG
+ * @param props - A object type SVGProps, with propieties pass to svg tag.
+ * @example
+ * import MenuIconSVG from 'components/icons/appbar/MenuIconSVG'
+ * <MenuIconSVG height={32} width={32} />
+ */
+const MenuIconSVG = (props: SVGProps<SVGSVGElement>): JSX.Element => (
+  <svg
+    aria-label='Menu Button'
+    role='button'
+    height={24}
+    width={24}
+    {...props}
+    viewBox='0 -53 384 384'
+  >
+    <path d='M368 155H16a16 16 0 010-32h352a16 16 0 010 32zm0-123H16a16 16 0 010-32h352a16 16 0 010 32zm0 245H16a16 16 0 010-32h352a16 16 0 010 32zm0 0' />
+  </svg>
+);
+
+export default MenuIconSVG;
