7db1f53cb51a25ecf6cecf38d9eac8be550aec47
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-09 17:33:44 -0300

    add typescript

--- src/components/icons/appbar/DarkIconSVG.tsx
+++ src/components/icons/appbar/DarkIconSVG.tsx
@@ -1,21 +1,20 @@
 /** @module Icons/AppBar */
+import { SVGProps } from 'react';
 /**
  * Use a icon with shape of moon in svg to make a dark theme button.
- * @function DarkIconSVG
  * @param {object} props - A object, with propieties pass to svg tag.
  * @example
  * import DarkIconSVG from 'components/icons/appbar/DarkIconSVG'
  * <DarkIconSVG height={32} width={32} />
- * @returns {JSX.Element} An element of React.
  */
-const DarkIconSVG = (props) => (
+const DarkIconSVG = (props: SVGProps<SVGSVGElement>): JSX.Element => (
   <svg
     aria-label='Dark Button'
     role='button'
     height={24}
     width={24}
-    {...props}
     viewBox='-12 0 500 500'
+    {...props}
   >
     <path d='M224 448c86 1 164-48 200-126-23 10-47 15-72 14A176 176 0 01271 3c-16-2-31-3-47-3a224 224 0 100 448zm0 0' />
   </svg>
