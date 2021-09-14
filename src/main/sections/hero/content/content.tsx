becc9a8d3528b037bc8c8a12abc3421e11300f36
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-14 05:13:11 -0300

    example content

---
+++ src/main/sections/hero/content/content.tsx
@@ -0,0 +1,137 @@
+import * as css from './content.module.css';
+/**
+ * A var type const for contain info set to display in section Hero.
+ * @const
+ * @example
+ * import content from 'main/sections/hero/content/content'
+ * content[0].title
+ * // return "NPM"
+ */
+const content = [
+  {
+    title: 'NPM',
+    caption: '¿Cómo pasamos de tres archivos, a montones de dependencias?',
+    html: (): JSX.Element => (
+      <>
+        <span className={css['prop']}>&quot;dependencies</span>&quot;: &#123;
+        <p>
+          &nbsp;&nbsp;<span className={css['prop']}>next</span>&quot;: &quot;
+          <span className={css['val']}>^11.1.2</span>&quot;,
+        </p>
+        <p>
+          &nbsp;&nbsp;<span className={css['prop']}>react</span>&quot;: &quot;
+          <span className={css['val']}>^17.0.2</span>&quot;,
+        </p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;. &nbsp; . &nbsp; .</p>
+      </>
+    ),
+  },
+  {
+    title: 'HTML 5',
+    caption: '¿Cuándo la semantica cambie, re-adaptaremos todas las webs?',
+    html: (): JSX.Element => (
+      <>
+        <span className={css['prop']}>&lt;div&gt;</span>Bad for sections
+        <span className={css['prop']}>&lt;/div&gt;</span>
+        <p>
+          <span className={css['prop']}>&lt;section&gt;</span>Good
+          <span className={css['prop']}>&lt;/section&gt;</span>
+        </p>
+        <p>
+          <span className={css['prop']}>&lt;p&gt;</span>Bad for Footer
+          <span className={css['prop']}>&lt;/p&gt;</span>
+        </p>
+        <p>
+          <span className={css['prop']}>&lt;footer&gt;</span>
+          <span className={css['prop']}>&lt;small&gt;</span>Good © Peter DG
+          <span className={css['prop']}>&lt;/small&gt;</span>
+          <span className={css['prop']}>&lt;/footer&gt;</span>
+        </p>
+      </>
+    ),
+  },
+  {
+    title: '<br />',
+    caption: '¿Hace cuánto que no utilizo algunas etiquetas?',
+    html: (): JSX.Element => (
+      <>
+        <span className={css['prop']}>
+          &lt;abbr title=
+          <span className={css['val']}>
+            &quot;World Health Organization&quot;
+          </span>
+        </span>
+        <span className={css['prop']}>&gt;</span>WHO
+        <span className={css['prop']}>&lt;/abbr&gt;</span>
+        <p>
+          <span className={css['prop']}>
+            &lt;base href=
+            <span className={css['val']}>
+              &quot;https://peterdg.com.ar/&quot;
+            </span>
+            &nbsp;/&gt;
+          </span>
+        </p>
+        <p>
+          <span className={css['prop']}>&lt;cite&gt;</span>You Aren&apos;t Gonna
+          Need It
+          <span className={css['prop']}>&lt;/cite&gt;</span>
+        </p>
+      </>
+    ),
+  },
+  {
+    title: 'Images',
+    caption: '¿Cuándo aparecerá el sistema de gráficos definitivo?',
+    html: (): JSX.Element => (
+      <>
+        <span className={css['prop']}>
+          &lt;canvas id=
+          <span className={css['val']}>&quot;myCanvas&quot;</span>
+          &gt;
+        </span>
+        Your browser does not support the canvas tag.
+        <span className={css['prop']}>&lt;/canvas&gt;</span>
+        <p>
+          <span className={css['prop']}>
+            &lt;svg width=
+            <span className={css['val']}>&quot;100&quot;</span>
+            <span className={css['prop']}>
+              &nbsp;height=<span className={css['val']}>&quot;100&quot;</span>
+            </span>
+            &gt;
+          </span>
+          <p>
+            &nbsp; &nbsp;
+            <span className={css['prop']}>
+              &lt;circle cx=
+              <span className={css['val']}>&quot;50&quot;</span>
+              <span className={css['prop']}>
+                &nbsp;cy=<span className={css['val']}>&quot;50&quot;</span>
+              </span>
+              <span className={css['prop']}>
+                &nbsp;r=<span className={css['val']}>&quot;50&quot;</span>
+              </span>
+              &nbsp;/&gt;
+            </span>
+          </p>
+          <p>
+            <span className={css['prop']}>&lt;/svg&gt;</span>
+          </p>
+        </p>
+        <p>
+          <span className={css['prop']}>
+            &lt;img src=
+            <span className={css['val']}>&quot;/profile.jpg&quot;</span>
+            <span className={css['prop']}>
+              &nbsp;alt=<span className={css['val']}>&quot;My photo&quot;</span>
+            </span>
+            &nbsp;/&gt;
+          </span>
+        </p>
+      </>
+    ),
+  },
+] as const;
+
+export default content;
