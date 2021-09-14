8016fe9627c0956d39e2a049087f744d282a67d6
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-14 00:54:40 -0300

    add strict mode

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    eslint: {
-      dirs: ['src'], // Only run ESLint on the 'src' directories during production builds (next build)
-    },
+  eslint: {
+    dirs: ['src'], // Only run ESLint on the 'src' directories during production builds (next build)
+  },
+  reactStrictMode: true,
   };
