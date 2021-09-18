901cd1adcb10638621827a768edce01defb55fc8
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-18 03:59:59 -0300

    certifications obj is data about certifications.

---
+++ src/main/sections/about/cert/certifications.ts
@@ -0,0 +1,59 @@
+/**
+ * A var type const, contain info to display list of certifications.
+ * @const
+ * @example
+ * import listCertification from 'main/sections/about/cert/listCertification'
+ * console.log(listCertification[0].title)
+ */
+const CERTIFICATIONS = [
+  {
+    id: 5,
+    title: 'JavaScript Algorithms and Data Structures',
+    emit: 'freeCodeCamp',
+    emitURL: 'https://www.linkedin.com/school/free-code-camp/',
+    credID: '',
+    URL: 'https://www.freecodecamp.org/certification/peterdg/javascript-algorithms-and-data-structures',
+  },
+  {
+    id: 4,
+    title: 'Aceleración FullStack en React, Node. Job Ready.',
+    emit: 'Alkemy',
+    emitURL: 'https://www.linkedin.com/company/alkemy2020/',
+    credID: '042-344',
+    URL: 'https://drive.google.com/file/d/1Ok8El_CKfCYyhfnkDs6v7FC7oIElLb9W/view',
+  },
+  {
+    id: 3,
+    title: 'Google Activate Introducción al Desarrollo Web (II)',
+    emit: 'Google Actívate',
+    emitURL: 'https://www.linkedin.com/company/activatecongoogle/',
+    credID: '2CG GHY F28',
+    URL: 'https://learndigital.withgoogle.com/activate/validate-certificate-code',
+  },
+  {
+    id: 2,
+    title: 'Google Activate Introducción al Desarrollo Web (I)',
+    emit: 'Google Actívate',
+    emitURL: 'https://www.linkedin.com/company/activatecongoogle/',
+    credID: 'VU5 LBF 85S',
+    URL: 'https://learndigital.withgoogle.com/activate/validate-certificate-code',
+  },
+  {
+    id: 1,
+    title: 'Google Activate Cloud Computing',
+    emit: 'Google Actívate',
+    emitURL: 'https://www.linkedin.com/company/activatecongoogle/',
+    credID: 'SJ5 A65 58Z',
+    URL: 'https://learndigital.withgoogle.com/activate/validate-certificate-code',
+  },
+  {
+    id: 0,
+    title: 'EF SET English Certificate 49/100 (B1 Intermediate)',
+    emit: 'EF Standard English Test (EF SET)',
+    emitURL: 'https://www.linkedin.com/company/efset/',
+    credID: '',
+    URL: 'https://www.efset.org/cert/QAFMtr',
+  },
+] as const;
+
+export default CERTIFICATIONS;
