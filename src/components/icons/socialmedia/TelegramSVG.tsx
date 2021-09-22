d3c298c7463159687ea69c65dda0adc88f66960a
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-22 16:27:59 -0300

    make two new component for socialmedia

---
+++ src/components/icons/socialmedia/TelegramSVG.tsx
@@ -0,0 +1,23 @@
+import { SVGProps } from 'react';
+/**
+ * Use a icon SVG like a Telegram Link button.
+ * @param props - A object type SVGProps, with propieties pass to svg tag.
+ * @example
+ * import TelegramSVG from 'components/icons/socialmedia/TelegramSVG'
+ * <TelegramSVG height={32} width={32} />
+ */
+const TelegramSVG = (props: SVGProps<SVGSVGElement>): JSX.Element => (
+  <svg
+    aria-label='Telegram Button'
+    role='button'
+    height={24}
+    width={24}
+    viewBox='0 0 512 512'
+    {...props}
+  >
+    <title>Enviarme un mensaje a Telegram.</title>
+    <path d='M12 24a12 12 0 100-24 12 12 0 000 24zM5.5 11.7L17 7.3c.5-.2 1 .1.8 1l-2 9.2c-.1.7-.5.8-1 .5l-3-2.2-1.5 1.4c-.2.2-.3.3-.6.3l.2-3 5.6-5c.2-.3 0-.4-.4-.2l-6.9 4.3-3-1c-.6-.1-.6-.5.2-.9z' />
+  </svg>
+);
+
+export default TelegramSVG;
---
+++ src/components/icons/socialmedia/WhatsAppSVG.tsx
@@ -0,0 +1,23 @@
+import { SVGProps } from 'react';
+/**
+ * Use a icon SVG like a WhatsApp Link button.
+ * @param props - A object type SVGProps, with propieties pass to svg tag.
+ * @example
+ * import WhatsAppSVG from 'components/icons/socialmedia/WhatsAppSVG'
+ * <WhatsAppSVG height={32} width={32} />
+ */
+const WhatsAppSVG = (props: SVGProps<SVGSVGElement>): JSX.Element => (
+  <svg
+    aria-label='WhatsApp Button'
+    role='button'
+    height={24}
+    width={24}
+    viewBox='0 0 512 512'
+    {...props}
+  >
+    <title>Enviarme un mensaje a WhatsApp.</title>
+    <path d='M256 0A256 256 0 0049 406l-32 95 98-31A256 256 0 10256 0zm149 362c-6 17-31 31-50 36-14 2-31 5-90-20-75-31-124-107-127-112-4-5-31-41-31-77s19-55 26-63c7-6 17-9 27-9h8c8 1 11 1 16 13l23 55c2 4 4 9 2 14l-9 12-11 12c-3 4-7 9-3 16 4 8 20 32 42 52 28 25 51 33 60 37 6 2 13 2 18-3l20-26c5-8 11-9 18-6 7 2 44 21 51 24 8 4 13 6 14 9 2 3 2 18-4 36z' />
+  </svg>
+);
+
+export default WhatsAppSVG;
