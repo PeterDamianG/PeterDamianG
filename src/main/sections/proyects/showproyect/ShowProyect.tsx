ed213d5abb33d0d7db944cd45468999a03ad2cac
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-23 16:00:39 -0300

    add nav and pagination to showproyect

--- src/main/sections/proyects/showproyect/ShowProyect.tsx
+++ src/main/sections/proyects/showproyect/ShowProyect.tsx
@@ -2,7 +2,10 @@ import { useState } from 'react';
 import useFunctionOverTime from 'hooks/useFunctionOverTime';
 import ShowProyectHotKeys from './ShowProyectHotKeys';
 import listProyects from './listProyects';
+import Button from '@components/ui/button/Button';
 import CardProyect from './card/CardProyect';
+import PaginationDot from './pagination/PaginationDot';
+import * as css from './showproyect.module.css';
 /**
  * A component to set up what proyects is render.
  * @example
@@ -13,31 +16,59 @@ const ShowProyect = (): JSX.Element => {
   // State of index about actual element.
   const [index, setIndex] = useState(0);
   // Handler for states.
-  const handleUpIndex = () =>
+  const handleUpIndex = (): void =>
     index === listProyects.length - 1
       ? setIndex(0)
       : setIndex((prevIndex) => prevIndex + 1);
-  const handleDownIndex = () =>
+  const handleDownIndex = (): void =>
     index === 0
       ? setIndex(listProyects.length - 1)
       : setIndex((prevIndex) => prevIndex - 1);
+  const handleSetIndex = (number: number): void => setIndex(number);
   // Hook to change states automatic.
   useFunctionOverTime(handleUpIndex, 10000);
   // Set hotkeys.
-  ShowProyectHotKeys(handleUpIndex, handleDownIndex);
+  ShowProyectHotKeys(handleUpIndex, handleDownIndex, index);
   // Destructuring.
   const { name, URL, description, tech, img } = listProyects[index];
   // Render.
   return (
-    <CardProyect
-      name={name}
-      URL={URL}
-      description={description}
-      tech={tech}
-      img={img}
-      upIndex={handleUpIndex}
-      downIndex={handleDownIndex}
-    />
+    <section className={css['container']}>
+      <Button
+        type='button'
+        className={css['slide-button']}
+        aria-label='Previous Button'
+        role='button'
+        onClick={handleDownIndex}
+        title='Retroceder al anterior proyecto. Acceso rápido flecha izquierda ←.'
+      >
+        &lt;
+      </Button>
+      <aside className={css['containerCardPag']}>
+        <CardProyect
+          name={name}
+          URL={URL}
+          description={description}
+          tech={tech}
+          img={img}
+        />
+        <PaginationDot
+          size={Array.from(Array(listProyects.length).keys())}
+          active={index}
+          setIndex={handleSetIndex}
+        />
+      </aside>
+      <Button
+        type='button'
+        className={css['slide-button']}
+        aria-label='Previous Button'
+        role='button'
+        onClick={handleUpIndex}
+        title='Avanzar al siguiente proyecto. Acceso rápido flecha derecha →.'
+      >
+        &gt;
+      </Button>
+    </section>
   );
 };
 
