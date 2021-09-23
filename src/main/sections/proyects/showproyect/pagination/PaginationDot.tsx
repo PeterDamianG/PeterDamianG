80a5ba213d295fa2a9ef1a296fc7e41243eb000c
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-23 16:00:01 -0300

    make a pagination dot component

---
+++ src/main/sections/proyects/showproyect/pagination/PaginationDot.tsx
@@ -0,0 +1,39 @@
+import BaseCard from '@components/ui/basecard/BaseCard';
+import Button from '@components/ui/button/Button';
+import * as css from './paginationdot.module.css';
+
+type PaginationDotProps = {
+  size: number[];
+  active: number;
+  setIndex: Function;
+};
+/**
+ * A component to render dots like a pagination.
+ * @example
+ * import PaginationDot from 'main/sections/showproyect/card/PaginationDot'
+ * <PaginationDot/>
+ */
+const PaginationDot = ({
+  size,
+  active,
+  setIndex,
+}: PaginationDotProps): JSX.Element => (
+  <BaseCard>
+    <div className={css['spaceButtonDot']}>
+      {size.map((number) => (
+        <Button
+          key={number}
+          className={
+            active === number ? css['buttonDotActive'] : css['buttonDot']
+          }
+          title={`Haz me click, para ir proyecto número: ${number}.`}
+          onClick={() => setIndex(number)}
+        >
+          &nbsp;
+        </Button>
+      ))}
+    </div>
+  </BaseCard>
+);
+
+export default PaginationDot;
