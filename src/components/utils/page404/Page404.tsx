473267aacc8b6692a1bb8254b7ae82cf16ccae0b
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-24 17:11:43 -0300

    make a new component to display in page 404

--- src/components/utils/page404/Page404.tsx
+++ src/components/utils/page404/Page404.tsx
@@ -1,15 +1,16 @@
-/** @module Pages */
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { mainStyle } from '../styles/404.module.css';
+import * as css from './page404.module.css';
 /**
- * A component for simulate a 500 page Server-side error occurred. Used same style like a 404.
- * @function Page500
+ * A component for simulate a 404 page error not found and redirect to index.
+ * @example
+ * import Page404 from 'components/utils/page404/Page404'
+ * <Page404 />
  */
-const Page500 = () => {
+const Page404 = (): JSX.Element => {
   const router = useRouter();
   // State for show info about time to redirect.
-  const [time, setTime] = useState(10);
+  const [time, setTime] = useState(5);
   // Effect timer down.
   useEffect(() => {
     setTimeout(() => {
@@ -23,19 +24,19 @@ const Page500 = () => {
   useEffect(() => {
     setTimeout(() => {
       router.push('/');
-    }, 10000);
+    }, 5000);
     return function cleanup() {
       clearTimeout();
     };
   }, [router]);
   // Render
   return (
-    <main className={mainStyle}>
-      <h2>500 - Un error del lado del servidor ha ocurrido.</h2>
+    <main className={css['page404']}>
+      <h2>404 - Página no encontrada.</h2>
       <p>Serás redireccionado a la página principal en {time} segundos.</p>
-      <h3>Lo siento, tuvimos un error desde el servidor.</h3>
+      <h3>Lo siento, la página que buscabas no se encuentra.</h3>
     </main>
   );
 };
 
-export default Page500;
+export default Page404;
---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import Page404 from '@components/utils/page404/Page404';
+/**
+ * A component for simulate a 404 page error not found and redirect to index.
+ */
+const Render404 = (): JSX.Element => <Page404 />;
+
+export default Render404;
