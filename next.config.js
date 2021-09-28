3034ca4b8e9e7c7d451a5b640d4d3cfe014d9c27
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-09-28 12:13:22 -0300

    config next, to use preact in production. Testing.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  eslint: {
-    dirs: ['src'], // Only run ESLint on the 'src' directories during production builds (next build)
-  },
   reactStrictMode: true,
+  webpack(config, { dev, isServer }) {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
+  },
 };
