68ef3ad04df892d70d662cff8df8d2bd303f8302
Peter Damián Gómez <peterdamiang@gmail.com>
2021-12-28 00:52:42 -0300

    add rust swc minification

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+  swcMinify: true,
   webpack(config, { dev, isServer }) {
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
